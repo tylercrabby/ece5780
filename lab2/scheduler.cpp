1d7f2b032f1b2abeb53f69647e7413c3951b9b36
Carter Nettesheim <carternetty@gmail.com>
2023-03-22 20:31:03 -0600

    file parsing works now

--- lab2/scheduler.cpp
+++ lab2/scheduler.cpp
@@ -1,36 +1,66 @@
 #include "scheduler.h"
 
-using namespace std;
-
-typedef struct {
-	char name;
-	int ex_time;
-	int period;
-} task;
-
 int main(int argc, char *argv[]) {
 	ifstream input;
 	ofstream output;
-	string temp_str;
 
 	input.open(argv[1]);
 	output.open(argv[2], ios::out);
 
 	// get number of tasks and simulation time
 	int num_tasks;
+	int num_a_tasks;
 	int sim_time;	
-	task tasks[num_tasks];
+	string temp_str;
+	vector<string> lines;
+	vector<task> tasks;
+	vector<task> a_tasks;
 	input >> num_tasks;
 	input >> sim_time;
+	
+	getline(input, temp_str);
 
 	for (int i = 0; i < num_tasks; i++) {
 		getline(input, temp_str);
-		stringstream parser(temp_str);
-		for (int j = 0; j < 3; j++) {
-			string s_string;
-			getline(parser, s_string, ',');
-			cout << s_string << endl;
-		}
+		lines.push_back(temp_str);
+	}
+
+	for (int i = 0; i < num_tasks; i++) {
+		stringstream ss(lines[i]);
+		task temp_t;
+		char comma;
+
+		ss >> temp_t.name >> comma >> temp_t.ex_time >> comma >> temp_t.period;
+		tasks.push_back(temp_t);
+	}
+
+	for (int i = 0; i < num_tasks; i++) {
+		cout << tasks[i].name << ' ' << tasks[i].ex_time << ' ' << tasks[i].period << endl;
+	}
+
+
+	// get number of asyncronous tasks
+	input >> num_a_tasks;
+
+	getline(input, temp_str);
+	lines.clear();
+
+	for (int i = 0; i < num_a_tasks; i++) {
+		getline(input, temp_str);
+		lines.push_back(temp_str);
+	}
+	
+	for (int i = 0; i < num_a_tasks; i++) {
+		stringstream ss(lines[i]);
+		task temp_t;
+		char comma;
+
+		ss >> temp_t.name >> comma >> temp_t.ex_time >> comma >> temp_t.period;
+		a_tasks.push_back(temp_t);
+	}
+	
+	for (int i = 0; i < num_a_tasks; i++) {
+		cout << a_tasks[i].name << ' ' << a_tasks[i].ex_time << ' ' << tasks[i].period << endl;
 	}
 
 	output.close();
--- lab2/scheduler.h
+++ lab2/scheduler.h
@@ -1,5 +1,18 @@
+#ifndef SCHEDULER_H
+#define SCHEDULER_H
 #include <stdio.h>
 #include <fstream>
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <vector>
+
+using namespace std;
+
+typedef struct {
+	char name;
+	int ex_time;
+	int period;
+} task;
+
+#endif
